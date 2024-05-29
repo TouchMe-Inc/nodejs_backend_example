b1ad8c8ed5d0c7b878dee1be332c4b11b9f94a6f
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-05-29 15:24:52 +0300

    mark news as Entity and add fields

--- src/entities/news.entity.ts
+++ src/entities/news.entity.ts
@@ -1,5 +1,11 @@
 import {Base} from "./base.entity";
+import {Column, Entity} from "typeorm";
 
+@Entity()
 export class News extends Base {
+    @Column({nullable: false})
+    title?: string;
 
+    @Column({nullable: false})
+    body?: string;
 }
