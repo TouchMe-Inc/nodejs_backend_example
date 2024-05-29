3661972021cd5f5bcc9dac7b627fc645a5c2741e
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-05-29 15:24:59 +0300

    mark user as Entity and add fields

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,5 +1,12 @@
 import {Base} from "./base.entity";
+import {Column, Entity, Index} from "typeorm";
 
+@Entity()
 export class User extends Base {
+    @Column()
+    @Index({unique: true})
+    login!: string;
 
+    @Column({select: false})
+    password!: string;
 }
