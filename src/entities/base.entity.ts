7f75c2806457028c2debc3cc3c599048e898c65e
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-05-29 18:31:08 +0300

    add PrimaryGeneratedColumn increment

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -1,7 +1,7 @@
 import {BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 
 export abstract class Base extends BaseEntity {
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn('increment')
     id!: number
 
     @CreateDateColumn({name: 'created_at', nullable: false})
