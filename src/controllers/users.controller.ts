5be9760181c81d7a1c57dc669dfde245a92a2982
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-05-29 18:34:07 +0300

    add simple method for get user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,16 +1,21 @@
 import {Get, JsonController, Param} from "routing-controllers";
 import {Service} from "typedi";
+import {UsersService} from "../services/users.service";
+import {User} from "../entities/user.entity";
 
 @JsonController('/users')
 @Service()
 export class UserController {
+    constructor(private readonly usersService: UsersService) {
+    }
+
     @Get()
     async index() {
         return {};
     }
 
     @Get('/:id')
-    async show(@Param('id') id: number) {
-        return {id};
+    show(@Param('id') id: number): Promise<User | null> {
+        return this.usersService.getById(id);
     }
 }
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,6 +1,15 @@
 import {Service} from "typedi";
+import {Repository} from "typeorm";
+import {dataSource} from "../database";
+import {User} from "../entities/user.entity";
 
 @Service()
 export class UsersService {
+    private readonly userRepository: Repository<User> = dataSource.getRepository(User);
 
+    getById(id: number) {
+        return this.userRepository.findOneBy({
+            "id": id
+        });
+    }
 }
