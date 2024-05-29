00aade6200e32efba30feb4b86b93647f3aaa7d8
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-05-29 15:21:13 +0300

    add fields: id and create/update time (with decorators)

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -1,3 +1,12 @@
-export abstract class Base {
+import {BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 
+export abstract class Base extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @CreateDateColumn({ name: 'created_at', nullable: false })
+    createdAt!: Date;
+
+    @UpdateDateColumn({ name: 'updated_at', nullable: false })
+    updatedAt!: Date;
 }
