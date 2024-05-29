52a8029cf5306935caf6ca97be547c61e95981e0
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-05-29 16:24:21 +0300

    add news migration

---
+++ src/database/migrations/1716988684892-CreateNews.ts
@@ -0,0 +1,45 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateNews1716988684892 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'user',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment'
+                    },
+                    {
+                        name: 'title',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'body',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            }),
+            true
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('news');
+    }
+
+}
