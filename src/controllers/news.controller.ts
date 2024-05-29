fea7014aa071403f6450d965b99085d1a967dcb4
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-05-29 18:34:12 +0300

    add simple method for get news

--- src/controllers/news.controller.ts
+++ src/controllers/news.controller.ts
@@ -1,11 +1,21 @@
-import {Get, JsonController} from "routing-controllers";
+import {Get, JsonController, Param} from "routing-controllers";
 import {Service} from "typedi";
+import {NewsService} from "../services/news.service";
+import {News} from "../entities/news.entity";
 
 @JsonController('/news')
 @Service()
 export class NewsController {
+    constructor(private readonly newsService: NewsService) {
+    }
+
     @Get()
     async index() {
-        return ["news10", "news11"];
+        return {};
+    }
+
+    @Get('/:id')
+    async show(@Param('id') id: number): Promise<News | null> {
+        return await this.newsService.getById(id);
     }
 }
--- src/services/news.service.ts
+++ src/services/news.service.ts
@@ -1,6 +1,15 @@
 import {Service} from "typedi";
+import {Repository} from "typeorm";
+import {dataSource} from "../database";
+import {News} from "../entities/news.entity";
 
 @Service()
 export class NewsService {
+    private readonly newsRepository: Repository<News> = dataSource.getRepository(News);
 
+    getById(id: number) {
+        return this.newsRepository.findOneBy({
+            "id": id
+        });
+    }
 }
