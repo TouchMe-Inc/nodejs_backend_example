dfa919307c50f7742ee9fa69e30af6888b89ca8e
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-05-31 21:07:16 +0300

    inject error middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,5 +2,6 @@ import {CorsMiddleware} from "./cors.middleware";
 import {HelmetMiddleware} from "./helmet.middleware";
 import {RateLimitMiddleware} from "./ratelimit.middleware";
 import {LoggerMiddleware} from "./logger.middleware";
+import {ErrorMiddleware} from "./error.middleware";
 
-export const middlewares = [CorsMiddleware, HelmetMiddleware, RateLimitMiddleware, LoggerMiddleware];
+export const middlewares = [CorsMiddleware, HelmetMiddleware, RateLimitMiddleware, LoggerMiddleware, ErrorMiddleware];
