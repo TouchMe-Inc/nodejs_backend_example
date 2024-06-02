904636949d6a23b62a8c0fe5c1f4d9ea9b5d6f88
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-06-02 23:41:42 +0300

    add methods for auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,4 +3,15 @@ import {Service} from "typedi";
 @Service()
 export class AuthService {
 
+    signUp() {
+        return [];
+    }
+
+    signIn() {
+        return 'token';
+    }
+
+    logOut() {
+
+    }
 }
