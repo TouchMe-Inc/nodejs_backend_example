e2e49ae6c5093821d064a000391b29f2c1c72f1b
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-06-03 22:59:27 +0300

    refactoring entities

--- src/entities/auth.entity.ts
+++ src/entities/auth.entity.ts
@@ -1,15 +1,20 @@
 import {Column, Entity, OneToMany} from "typeorm";
 import {Base} from "./base.entity";
-import {AuthIdentity} from "./authIdentity.entity";
 import {Session} from "./session.entity";
 
 @Entity("auth")
 export class Auth extends Base {
-    @Column()
+    @Column({name: "user_id", nullable: false})
     userId!: number;
 
-    @OneToMany(type => AuthIdentity, authIdentity => authIdentity.authId)
-    identities?: AuthIdentity[];
+    @Column({name: "provider_name", nullable: false})
+    providerName!: string;
+
+    @Column({name: "provider_id"})
+    providerId!: string;
+
+    @Column({name: "provider_data"})
+    providerData?: string;
 
     @OneToMany(type => Session, session => session.authId)
     session?: Session[];
--- src/entities/session.entity.ts
+++ src/entities/session.entity.ts
@@ -3,12 +3,12 @@ import {Base} from "./base.entity";
 
 @Entity("session")
 export class Session extends Base {
-    @Column()
-    userId!: number;
-
-    @Column()
+    @Column({name: "auth_id", nullable: false})
     authId!: number;
 
-    @Column()
-    expiresAt?: Date;
+    @Column({name: "user_id", nullable: false})
+    userId!: number;
+
+    @Column({name: 'expires_at', nullable: false})
+    expiresAt!: Date;
 }
