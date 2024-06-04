42661a25c39dde84984a91eec86fca8e5c7c820a
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-06-04 14:44:31 +0300

    add decorator "Authorized" for users

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Delete, Get, JsonController, Param, Post, Put} from "routing-controllers";
+import {Authorized, Body, Delete, Get, JsonController, Param, Post, Put} from "routing-controllers";
 import {plainToClass} from 'class-transformer';
 import {Service} from "typedi";
 import {UsersService} from "../services/users.service";
@@ -12,16 +12,19 @@ export class UserController {
     constructor(private readonly usersService: UsersService, private readonly passwordService: PasswordService) {
     }
 
+    @Authorized()
     @Get()
     async index() {
         return this.usersService.getAll();
     }
 
+    @Authorized()
     @Get('/:id')
     async show(@Param('id') id: number): Promise<User | null> {
         return this.usersService.getById(id);
     }
 
+    @Authorized(["admin"])
     @Post()
     async create(@Body({validate: true}) createDto: UserDto) {
         const user = plainToClass(User, createDto);
@@ -30,6 +33,7 @@ export class UserController {
         return this.usersService.create(user);
     }
 
+    @Authorized(["admin"])
     @Put('/:id')
     async update(@Param('id') id: number, @Body({validate: true}) updateDto: UserDto) {
         const user = plainToClass(User, updateDto)
@@ -37,6 +41,7 @@ export class UserController {
         return this.usersService.updateById(id, user);
     }
 
+    @Authorized(["admin"])
     @Delete('/:id')
     async delete(@Param('id') id: number) {
         return this.usersService.deleteById(id);
