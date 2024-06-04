f2e9914b781af70e449584ca15f652773d2d9aa0
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-06-04 13:48:16 +0300

    update config vars

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,15 +2,15 @@ import dotnet from 'dotenv';
 
 dotnet.config();
 
-export const ENV_PROD = process.env.ENV === 'prod';
+export const ENV_PROD: boolean = process.env.ENV === 'prod';
+export const TYPEORM_SYNCHRONIZE: boolean = process.env.TYPEORM_SYNCHRONIZE === 'true';
+export const TYPEORM_LOGGING: boolean = process.env.TYPEORM_LOGGING === 'true';
 
 export const {
     PORT,
     RATE_LIMIT_WINDOW,
     RATE_LIMIT_MAX_REQUESTS,
     TYPEORM_DATABASE,
-    TYPEORM_SYNCHRONIZE,
-    TYPEORM_LOGGING,
     TYPEORM_MIGRATIONS_RUN,
     JWT_SECRET,
     JWT_TOKEN_LIFE
