a20f3060dae36f06cf3c4f3886c4585cd03ab6fe
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-06-04 13:48:00 +0300

    add jwt async

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -5,17 +5,40 @@ import {JWT_SECRET, JWT_TOKEN_LIFE} from "../config";
 @Service()
 export class JwtService {
 
-    sign(payload: any) {
-        return jwt.sign(payload, JWT_SECRET || "", {
-            expiresIn: JWT_TOKEN_LIFE
+    async sign(payload: any) {
+        return new Promise((resolve, reject) => {
+            try {
+                const token = jwt.sign(payload, JWT_SECRET as string, {
+                    expiresIn: JWT_TOKEN_LIFE
+                });
+                resolve(token);
+            } catch (error) {
+                reject(error);
+            }
         });
     }
 
-    decode(token: string) {
-        return jwt.decode(token);
+    async decode(token: string) {
+        return new Promise((resolve, reject) => {
+            try {
+                const decoded = jwt.decode(token);
+
+                resolve(decoded);
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
 
-    verify(token: string) {
-        return jwt.verify(token, JWT_SECRET || "");
+    async verify(token: string) {
+        return new Promise((resolve, reject) => {
+            try {
+                const decoded = jwt.verify(token, JWT_SECRET as string);
+
+                resolve(decoded);
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
 }
