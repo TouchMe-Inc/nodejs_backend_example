7273f49765273be8d46865d2e53eb8801756693f
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-06-04 14:44:16 +0300

    add authorizationChecker for decorator "Authorized"

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,42 @@
 import 'reflect-metadata';
 import express from 'express';
-import {getMetadataArgsStorage, useContainer, useExpressServer} from 'routing-controllers';
+import {Action, getMetadataArgsStorage, useContainer, useExpressServer} from 'routing-controllers';
 import {Container} from 'typedi';
 import {controllers} from './controllers';
 import {middlewares} from "./middlewares";
 import {useSwagger} from "./swagger";
+import {JwtService} from "./services/jwt.service";
+import {UsersService} from "./services/users.service";
+import {RoutingControllersOptions} from "routing-controllers/types/RoutingControllersOptions";
 
 const app: express.Express = express();
 
-const  routingControllersOptions = {
+useContainer(Container);
+
+const routingControllersOptions: RoutingControllersOptions = {
     routePrefix: '/api',
     defaultErrorHandler: false,
     interceptors: [],
     controllers,
-    middlewares
+    middlewares,
+    authorizationChecker: async (action: Action, roles: string[]) => {
+        const token = action.request.headers['authorization'];
+
+        const payload = await Container.get(JwtService).verify(token);
+
+        // @ts-ignore
+        const userId = payload.id;
+
+        const user = await Container.get(UsersService).getById(userId);
+
+        if (!user) {
+            return false;
+        }
+
+        return !!(!roles.length || roles.find(role => user.role === role));
+    },
 }
 
-useContainer(Container);
 
 useExpressServer(app, routingControllersOptions);
 
